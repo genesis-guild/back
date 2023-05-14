2ede62a80f2b4fcbbab389e3f4b97c395273caf4
Daniil Khalupka <48531462+ordinaryask@users.noreply.github.com>
2023-05-14 13:18:37 +0200

    feat: account dashboard endopoints (#2)

--- src/modules/account/index.ts
+++ src/modules/account/index.ts
@@ -4,6 +4,7 @@ import { BadgeModule } from 'modules/badge'
 import { ETHService } from 'services'
 
 import { AccountController } from './controller'
+import { AccountGameDto, AccountGameSchema } from './dto/account-game.dto'
 import { LenderDto, LenderSchema } from './dto/lender.dto'
 import { PlayerDto, PlayerSchema } from './dto/player.dto'
 import { AccountDto, AccountSchema } from './dto/user.dto'
@@ -13,6 +14,9 @@ export const Models = [
   MongooseModule.forFeature([{ name: AccountDto.name, schema: AccountSchema }]),
   MongooseModule.forFeature([{ name: LenderDto.name, schema: LenderSchema }]),
   MongooseModule.forFeature([{ name: PlayerDto.name, schema: PlayerSchema }]),
+  MongooseModule.forFeature([
+    { name: AccountGameDto.name, schema: AccountGameSchema },
+  ]),
 ]
 
 @Module({
